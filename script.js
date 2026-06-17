588c40d2990122e9af2e4c9c0cce8304f75f3690
ProCode7-star <hansel.rocha.17@icloud.com>
2026-06-16 21:19:37 -0600

    Crear script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Esta función se ejecuta automáticamente cuando Google aprueba el reCAPTCHA
+function alVerificar(token) {
+    console.log("¡Verificación exitosa! Token:", token);
+    
+    // 🚀 Aquí pones directamente lo que quieres que pase.
+    // Por ejemplo, puedes llamar a la función que muestra tus APIs:
+    // mostrarMisApis();
+}
